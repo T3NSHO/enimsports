1b0f66ee42028fc8fbee617478acc0a8156eb446
T3NSHO <amineos726@gmail.com>
2024-11-04 16:38:10 +0100

    fixed few styling errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+
+import HeroSection from "@/components/HeroSection";
+import "./home.css";
 export default function Home() {
   return (
-  <div>
+  <div className="background">
     <Navbar />
+    <HeroSection />
+    
   </div>  
   );
 }
